04db223e28f548ddd489a1888e9e1525a998cf9d
Rafael da Silva Siqueira <rafaeldasilvasiqueira@yahoo.com.br>
2025-06-02 20:00:02 -0300

    criado a base do script de monitoramento

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Monitoramento-Linux
+Monitoramento de Serviços de servidor LINUX
+
+scrript padrão para monitoramento base para sistemas linux que verifica:
+
+Status de serviços ativos
+Uso de CPU, Memoria e Disco
+Geração de log em '/var/log/monitoramento.log'
+
+Pasta para execução do script:
+o script ficará em '/home/customização/monitoramento.sh'
+
+*********************************************************************************8
+
+Como utilizar
+
+Atribuir permição para execução do script
+
+chmod +x monitoramento.sh
+
+Poderá ser executado de maneira manual:
+./monitoramento.sh
+
+Agendada utilizando cron a cada 5 minutos podendo agendar em menos tempo
+*/5**** /home/customização/monitoramento.sh
---
+++ monitoramento.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Configurações Base
+SERVICO=()
+LOG="/var/log/monitoramento.log"
+LIMITE_CPU=80
+LIMITE_MEMORIA=80
+LIMITE_DISCO=84
+
+echo "[$(date '+%d/%m/%Y %H:%M:%S')] Inicio monitoramento" >> $LOG
+
+#Confirmando os serviço
+for SERVICO in "${SERVICOS[@]}"; do STATUS = $(systemctl is-active
+$SERVICO)
+    if [ "Status" != "active" ];
+then
+    echo "[$(date)] ALERTA:
+serviços $SERVICO está $STATUS" >> $LOG
+    fi
+done
+
+#Uso de CPU
+USO_CPU=$(top -bn1 | grep "Cpu" | awk '{print 100 -$8}')
+if (($(echo "$USO_CPU > $LIMITE_CPU" | bc -1) )); then
+    echo "[$(date)] ALERTA: uso de CPU alto: $USO_CPU%" >> $LOG
+fi
+
+#Uso da Memoria
+USO_MEMORIA=$(free | aws '/Mem/ {printf("%.2f"), $3/$2 * 100.0}')
+if (( $(echo "$USO_MEMORIA > $LIMITE_MEMORIA" | bc -1) )); then
+    echo "[$(date)] ALERTA: Uso de memoria alto: $USO_MEMORIA%" >> $LOG
+fi
+
+#Uso do Disco
+Uso_DISCO=$(df / | aws 'NR==2 {gsub("%", ""); print $5}')
+if [ "$USO_DISCO" -gt "$LIMITE_DISCO" -gt "$LIMITE_DISCO" ]; then
+    echo "[$(date)] ALERTA: uso de Disco alto: $USO_DISCO%" >> $LOG
+fi
+
+echo "[$(date '+%d/%m/%Y %H:%M:%S')]
+Fim do monitoramento" >> $LOG
+echo "__________________________" >> $LOG
